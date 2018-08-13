ebda49e3f6f50311e16903ec2ea7683d3f381105
wiztigers <regis.witz@free.fr>
2018-08-13 21:33:51 +0200

    those lines shouldn't have been deleted in c7151b ^^; ...

--- setup.sh
+++ setup.sh
@@ -7,10 +7,19 @@ stylename=$2
 [ -z ${stylesdir} ] && stylesdir=sass
 [ -d ${stylesdir} ] || mkdir ${stylesdir}
 
+# asciidoctor & dependencies
+gem install prawn -v 2.1.0
+gem install asciidoctor # HTML generation
+gem install asciidoctor-pdf --pre # PDF generation
+
 # get stylesheet factory
+gem install compass zurb-foundation # CSS stylesheets generation
 [[ -d ${factorydir} ]] || git clone https://github.com/asciidoctor/asciidoctor-stylesheet-factory.git ${factorydir}
 # generate CSS stylesheet
 cp -R ${stylesdir} ${factorydir}/
 cd ${factorydir} && compass compile ${stylesdir}/${stylename}.scss && cd ..
 cp ${factorydir}/stylesheets/${stylename}.css ${stylesdir}/${stylename}.css
 
+# get the latest version of wkhtmltopdf and untar it
+wget https://github.com/wkhtmltopdf/wkhtmltopdf/releases/download/0.12.4/wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
+tar xf wkhtmltox-0.12.4_linux-generic-amd64.tar.xz
