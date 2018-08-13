6a2f351c357aa3743cf8e386d1f0d974d17f1485
wiztigers <regis.witz@free.fr>
2018-08-13 18:19:16 +0200

    upgrade deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-echo ${PATH}
+AUTHOR_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%aN")"
+AUTHOR_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%aE")"
+COMMITTER_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%cN")"
+COMMITTER_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%cE")"
+
 # commit the new website version and push it
 cd gh-pages; git config user.name "traviscibot"; git config user.email "deploy.well_not_exactly@travis-ci.org"
 git add .; git commit --author="${AUTHOR_NAME} <${AUTHOR_EMAIL}>" -m "Deploy ${TRAVIS_COMMIT_RANGE} to github pages"
