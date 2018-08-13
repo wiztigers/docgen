36222d6eedfd6a2a2cea6ef6aed372ff905c6e15
wiztigers <regis.witz@free.fr>
2018-08-13 18:34:18 +0200

    cleanup

--- deploy.sh
+++ deploy.sh
@@ -5,16 +5,8 @@ AUTHOR_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%aE")"
 COMMITTER_NAME="$(git log -1 $TRAVIS_COMMIT --pretty="%cN")"
 COMMITTER_EMAIL="$(git log -1 $TRAVIS_COMMIT --pretty="%cE")"
 
-echo begin
-echo ${AUTHOR_NAME}
-echo ${AUTHOR_EMAIL}
-echo ${COMMITTER_NAME}
-echo ${COMMITTER_EMAIL}
-echo ${TRAVIS_REPO_SLUG}
-echo end
-
 # commit the new website version and push it
-cd gh-pages; git config user.name "traviscibot"; git config user.email "deploy.well_not_exactly@travis-ci.org"
+cd gh-pages; git config user.name "traviscibot"; git config user.email "deploy@travis-ci.org"
 git add .; git commit --author="${AUTHOR_NAME} <${AUTHOR_EMAIL}>" -m "Deploy ${TRAVIS_COMMIT_RANGE} to github pages"
 git push --quiet "https://${GITHUB_TOKEN}@github.com/${TRAVIS_REPO_SLUG}" gh-pages > /dev/null 2>&1
 cd ..
